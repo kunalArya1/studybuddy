f8e9dd16238bfa1acba3fe830cbd95ccb59bce53
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2026-02-07 16:32:36 +0530

    feat(db): schema added and migrated the db

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,9 +4,11 @@ import express, {
   type Response,
 } from "express";
 
+import authRouter from "./routes/auth.routes.js";
 
 const app: Application = express();
 
+app.use("/auth", authRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.json({
