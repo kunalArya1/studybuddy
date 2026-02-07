5602a4e959ad2f7b4f070c3f584d537ce2a40030
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2026-02-07 11:37:55 +0530

    chore(setup): Server setup is done

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+
+const app: Application = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    message: "Hello from server",
+  });
+});
+
+export default app;
