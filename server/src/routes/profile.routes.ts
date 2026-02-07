438d64932f9a58195c06f64644ea8921695f3790
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2026-02-07 17:30:36 +0530

    refactor(profile routes): controller added

--- server/src/routes/profile.routes.ts
+++ server/src/routes/profile.routes.ts
@@ -1,4 +1,12 @@
 import { Router } from "express";
+import {
+  deleteProfile,
+  enrolledCourses,
+  getUserDetails,
+  instructorDashboard,
+  updateProfile,
+  updateProfileImage,
+} from "../controllers/profile.controller.js";
 
 const router: Router = Router();
 
@@ -7,38 +15,37 @@ const router: Router = Router();
  * @desc  Get logged-in user details
  * @access Private
  */
-router.route("/get-user-details").get();
+router.route("/get-user-details").get(getUserDetails);
 
 /**
  * @route PUT /update-profile
  * @desc  Update user profile details
  * @access Private
  */
-router.route("update-profile").put();
+router.route("update-profile").put(updateProfile);
 /**
  * @route DELETE /delete-profile
  * @desc  Delete user profile
  * @access Private
  */
-router.route("delete-profile").delete();
+router.route("delete-profile").delete(deleteProfile);
 /**
  * @route GET /enrolled-course
  * @desc  Get all courses enrolled by the user
  * @access Private
  */
-router.route("/enrolled-course").get();
+router.route("/enrolled-course").get(enrolledCourses);
 /**
  * @route PUT /update-profile-image
  * @desc  Update user profile image
  * @access Private
  */
-router.route("update-profile-image").put();
+router.route("update-profile-image").put(updateProfileImage);
 /**
  * @route GET /instructor-dashboard
  * @desc  Get instructor dashboard analytics and data
  * @access Private (Instructor only)
  */
-router.route("instructor-dashboard").get();
-
+router.route("instructor-dashboard").get(instructorDashboard);
 
 export default router;
